316c5643051a93c8af7f5f9ef0b429b142661a86
vsokolovskyi <effrevescent@gmail.com>
2018-11-06 13:13:44 +0200

    initial work

--- src/test/java/onseo/jerseytest/MockSampleTest.java
+++ src/test/java/onseo/jerseytest/MockSampleTest.java
@@ -19,7 +19,6 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MockSampleTest {
-
     //Mocks
     @Mock
     private PlaceholderClient<ToDoEntity> toDoEntityPlaceholderClient;
